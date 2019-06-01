bf206a44e4932815863bc2e28963c074d3ccebba
Olga Rosas <olga.rosas@gmail.com>
2019-06-01 11:02:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -21,5 +21,9 @@ def last_element(array)
 end
 
 def first_element_with_array_methods(array)
-  array.shift
+  return array.shift
+end
+
+def last_element_with_array_methods(array)
+  return array.pop
 end
