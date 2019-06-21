2cfc83c4bba41c31bbeb49bc61a6d77bde762321
Ishmet <ishmetjahan@yahoo.com>
2019-06-21 22:35:04 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,31 @@
+def instantiate_new_array 
+return []
+end
+#
+def array_with_two_elements 
+  new_arr = ["happy",7]
+end
+#
+def frist_element (arr)
+ return arr[0]
+end
+#
+def third_element (arr)
+return arr[2]
+end
+#
+def last_element (arr)
+return arr[arr.length-1]
+end
+#
+def first_element_with_array_methods (arr)
+return arr.first
+end
+#
+def last_element_with_array_methods(arr)
+  return arr.last
+end
+#
+def length_of_array (arr)
+  return arr.length
+end
